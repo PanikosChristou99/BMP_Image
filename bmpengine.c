9a8a490d92f102e9a51a7a0b3822bbcf3baf7a0e
stylianos <stylianos@stylianos-VirtualBox>
2019-11-17 17:20:47 +0200

    stylianos

--- bmpengine.c
+++ bmpengine.c
@@ -12,8 +12,6 @@ int main(int argc, char **argv) {
 int checkArgsFor(char *str, int argc, char **argv) {
 	int i = 0;
 	for (i = 0; i < argc; ++i) {
-		if (strcmp(argv[i], str) == 0)
-			return i;
 	}
 	return -1;
 
