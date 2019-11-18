fba6166f5dc726ab8b0361c2afba80e950cedd33
Stylianos Herodotou <49488837+sherod01@users.noreply.github.com>
2019-11-18 17:47:30 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -60,6 +60,42 @@ void hflip (bmp_image *original)
 	ans->nameOfFile=tempC;
 	printInBinaryFile(ans);
 }
+void vflip (bmp_image *original)
+{
+	image_header* header= copyHeader(original->header);
+	image_data *data = (image_data*) malloc(sizeof(image_data));
+	int height=original->header->infoHeader.biHeight;
+	int width= original->header->infoHeader.biWidth;
+	int padding = (width * 3) % 4;
+	data->pixelArray = (Pixel***) malloc( height * sizeof(Pixel**));
+
+
+	for (int i =0; i <height; i++) {
+		int z=width-1;
+		data->pixelArray[i] = (Pixel**) malloc((width+padding) * sizeof(Pixel*));
+		for (int j = 0; j < width; j++) {
+			data->pixelArray[i][j] = (Pixel*) malloc(sizeof(Pixel));
+			data->pixelArray[i][j] = copyPixel(original->data->pixelArray[i][z]);
+			//printPixel(data->pixelArray[i][j]);
+			z--;
+		}
+		for (int j = 0; j < padding; j++) {
+			data->pixelArray[i][width+j] = (Pixel*) malloc(sizeof(Pixel));
+			data->pixelArray[i][width+j] = makePaddingPixel();
+			//printPixel(temp);
+		}
+
+	}
+	bmp_image* ans= (bmp_image*)malloc(sizeof(bmp_image));
+	ans->header=header;
+	ans->data=data;
+	char* tempC= (char*) malloc((strlen(original->nameOfFile)+7)*sizeof(char));
+	strcpy(tempC,"vflip-");
+	strcat(tempC,original->nameOfFile);
+	printf("name of original %s\n",original->nameOfFile);
+	ans->nameOfFile=tempC;
+	printInBinaryFile(ans);
+}
 void zoomout(bmp_image *prev){
 	double_word width = prev->header->infoHeader.biWidth;
 		double_word height = prev->header->infoHeader.biHeight;
