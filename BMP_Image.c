56ef42565db52585779981767cafd8287d5c7fef
Stylianos Herodotou <49488837+sherod01@users.noreply.github.com>
2019-11-18 16:24:35 +0000

    Update BMP_Image.c

--- BMP_Image.c
+++ BMP_Image.c
@@ -54,5 +54,47 @@ void printImageNot(bmp_image * pic)
 	printHeader(pic->header);
 	printData(pic->data,pic->header->infoHeader.biHeight,pic->header->infoHeader.biWidth);
 }
+image_header* copyHeader(image_header* original)
+{
+	// tuta dame epd enen pointers simeni pos anigrafounte sosta?
+	image_header * temp = (image_header*)malloc(sizeof(image_header));
+	// gia to fileHeader
+	temp->fileHeader.bfOffBits=original->fileHeader.bfOffBits;
+	temp->fileHeader.bfReserved1=original->fileHeader.bfReserved1;
+	temp->fileHeader.bfReserved2=original->fileHeader.bfReserved2;
+	temp->fileHeader.bfSize=original->fileHeader.bfSize;
+	temp->fileHeader.bfType1=original->fileHeader.bfType1;
+	temp->fileHeader.bfType2=original->fileHeader.bfType2;
+	// gia to infoHEader
+	temp->infoHeader.biBitCount=original->infoHeader.biBitCount;
+	temp->infoHeader.biClrImportant=original->infoHeader.biClrImportant;
+	temp->infoHeader.biClrUsed=original->infoHeader.biClrUsed;
+	temp->infoHeader.biCompression=original->infoHeader.biCompression;
+	temp->infoHeader.biHeight=original->infoHeader.biHeight;
+	temp->infoHeader.biPlanes=original->infoHeader.biPlanes;
+	temp->infoHeader.biSize=original->infoHeader.biSize;
+	temp->infoHeader.biSizeImage=original->infoHeader.biSizeImage;
+	temp->infoHeader.biWidth=original->infoHeader.biWidth;
+	temp->infoHeader.biXPelsPerMeter=original->infoHeader.biXPelsPerMeter;
+	temp->infoHeader.biYPelsPerMeter=original->infoHeader.biYPelsPerMeter;
+	return temp;
+}
+
+Pixel* copyPixel(Pixel* original)
+{
+	Pixel* temp = (Pixel*)malloc(sizeof(Pixel));
+	temp->blue=original->blue;
+	temp->green=original->green;
+	temp->red=original->red;
+	return temp;
+}
+Pixel * makePaddingPixel()
+{
+	Pixel * pad= (Pixel*)malloc(sizeof(Pixel));
+	pad->red=(unsigned char) 0;
+	pad->green=(unsigned char) 0;
+	pad->blue=(unsigned char) 0;
+	return pad;
+}
 
 
