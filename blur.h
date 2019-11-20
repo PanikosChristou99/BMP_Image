575f3d6d7b4a7493b33a28caab75d9d8359b4317
Panikos Christou <panikoschristou@yahoo.com>
2019-11-20 16:00:08 +0200

    Add files via upload

---
+++ blur.h
@@ -0,0 +1,14 @@
+/*
+ * blur.h
+ *
+ *  Created on: Nov 20, 2019
+ *      Author: panikos
+ */
+
+#ifndef BLUR_H_
+#define BLUR_H_
+void blur(bmp_image *prev);
+Pixel*  blurcalcSharpenValues(bmp_image* image, int x, int y) ;
+
+
+#endif /* BLUR_H_ */
