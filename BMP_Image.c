56f7151b33136484e24d32a4f0ec2be305989890
Stylianos Herodotou <49488837+sherod01@users.noreply.github.com>
2019-11-20 11:20:57 +0000

    Update BMP_Image.c

--- BMP_Image.c
+++ BMP_Image.c
@@ -25,12 +25,14 @@ void printInBinaryFile( bmp_image* image)
 				{
 					fwrite((image->data->pixelArray[i][j]), sizeof(Pixel), 1, whereToPrint);
 				}
-				fwrite(makePaddingbyte(), sizeof(byte), padding, whereToPrint);
-
+				for(int j=0; j<padding; j++)
+				{
+					fwrite(makePaddingbyte(), sizeof(byte), padding, whereToPrint);
+				}
 			}
 		fclose(whereToPrint);
 }
-byte * makePaddingbyte()
+byte * makePaddingbyte() // dame pezi na exume lathos
 {
 	byte* pad= (byte*)malloc(sizeof(byte));
 	*(pad) = (byte)0;
@@ -154,24 +156,21 @@ bmp_image* readBmp(char *fileName) {
 	data->pixelArray = (Pixel***) malloc(height * sizeof(Pixel**));
 	Pixel *temp = malloc(sizeof(Pixel));
 	for (int i = height-1; i >=0; i--) {
-		data->pixelArray[i] = (Pixel**) malloc((width+padding) * sizeof(Pixel*));
-		for (int j = 0; j < width + padding; j++) {
-			if (j< width) {
+		data->pixelArray[i] = (Pixel**) malloc((width) * sizeof(Pixel));
+		for (int j = 0; j < width; j++) {
 				data->pixelArray[i][j] = (Pixel*) malloc(sizeof(Pixel));
 				fread(data->pixelArray[i][j], sizeof(Pixel), 1, bin);
 				//printPixel(data->pixelArray[i][j]);
-			} else {
-				fread(temp, sizeof(byte), 1, bin);
-			}
-			//printPixel(temp);
-
 		}
-	}
-
+		for(int j=0; j<padding; j++){
+				fread(temp, sizeof(byte), 1, bin);
+		}
+		}
 	bmp_image *image = (bmp_image*) malloc(sizeof(bmp_image));
 	image->data = data;
 	image->header = header;
 	image->nameOfFile = fileName;
+	fclose(bin);
 	return image;
 
 }
