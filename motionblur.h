5014f8bc24a117e688c4a6893d8edee5c088e1d1
Panikos Christou <panikoschristou@yahoo.com>
2019-11-20 16:04:48 +0200

    Add files via upload

---
+++ motionblur.h
@@ -0,0 +1,14 @@
+/*
+ * motionblur.h
+ *
+ *  Created on: Nov 20, 2019
+ *      Author: panikos
+ */
+
+#ifndef MOTIONBLUR_H_
+#define MOTIONBLUR_H_
+
+void motionblur(bmp_image *prev);
+Pixel*  motionblurcalcSharpenValues(bmp_image* image, int x, int y) ;
+
+#endif /* MOTIONBLUR_H_ */
